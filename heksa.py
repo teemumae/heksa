ad4534e0c1e6e6ca4c9b01334774596b847ba597
Teemu Mäenpää <teemumaen@gmail.com>
2020-10-01 19:08:18 +0300

    toinen yritys

--- heksa.py
+++ heksa.py
@@ -1,7 +1,7 @@
 def muotoile_heksaluvuksi(luku, bitit):
     heksa = hex(luku)
     bitit = bitit//4
-    m_heksa = heksa.replace("x", "0")
+    m_heksa = heksa.replace("0x", "")
     m_heksa = m_heksa.zfill(bitit)
     return m_heksa
 try:
